c04116ef8d5bd28d741b3cf23ffbf996127cbfa1
dphan <brianp.dung@gmail.com>
2016-10-07 18:48:40 -0500

    Turned off visualization, forcing command line run control.

--- LSsim.cc
+++ LSsim.cc
@@ -13,9 +13,9 @@
 
 #include "QGSP_BIC_HP.hh"
 
-#ifdef G4VIS_USE
-#include "G4VisExecutive.hh"
-#endif
+//#ifdef G4VIS_USE
+//#include "G4VisExecutive.hh"
+//#endif
 
 #ifdef G4UI_USE
 #include "G4UIExecutive.hh"
@@ -34,10 +34,10 @@ int main(int argc, char ** argv) {
 
   runManager->SetUserInitialization(new LSActionInitialization());
 
-  #ifdef G4VIS_USE
-  G4VisManager * visManager = new G4VisExecutive;
-  visManager->Initialize();
-  #endif
+  //#ifdef G4VIS_USE
+  //G4VisManager * visManager = new G4VisExecutive;
+  //visManager->Initialize();
+  //#endif
 
   runManager->Initialize();
 
@@ -47,9 +47,9 @@ int main(int argc, char ** argv) {
   if (argc == 1) {
     #ifdef G4UI_USE
     G4UIExecutive * ui = new G4UIExecutive(argc, argv);
-    #ifdef G4VIS_USE
-    UImanager->ApplyCommand("/control/execute vis.mac");
-    #endif
+    //#ifdef G4VIS_USE
+    //UImanager->ApplyCommand("/control/execute vis.mac");
+    //#endif
     if (ui->IsGUI()) {
       UImanager->ApplyCommand("/control/execute gui.mac");
     }
@@ -62,9 +62,9 @@ int main(int argc, char ** argv) {
     UImanager->ApplyCommand(command + filename);
   }
 
-  #ifdef G4VIS_USE
-  delete visManager;
-  #endif
+  //#ifdef G4VIS_USE
+  //delete visManager;
+  //#endif
 
   delete runManager;
 
